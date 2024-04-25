98881bdc1e2fe2f89774a6f34850841f24deeeb3
marklar <ddillon@gmail.com>
2024-04-25 17:49:55 -0400

    Added basic models to admin

--- werminal/trimlo/admin.py
+++ werminal/trimlo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from trimlo.models import Board, Column, Card
 
 # Register your models here.
+
+admin.site.register(Board)
+admin.site.register(Column)
+admin.site.register(Card)
